eba3b5372afd49b9bcd516fc7133ebdb89fe0fe8
scorpion2000 <robert.bartha1@gmail.com>
2024-08-17 21:49:02 +0200

    Initial item pricing

--- scripts/items/item_global.gd
+++ scripts/items/item_global.gd
@@ -3,3 +3,8 @@ extends Node
 class_name ItemGlobal
 
 enum FoodTypes {CANNED, CEREAL}
+
+var FoodValues: Dictionary = {
+    FoodTypes.CANNED : {purchasePrice = 1, sellPrice = 2},
+    FoodTypes.CEREAL : {purchasePrice = 1, sellPrice = 2}
+}
