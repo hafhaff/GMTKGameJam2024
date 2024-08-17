450920e581d832643d65f44500c249c2058af966
NathanTheCraziest <nathanthecraziest@gmail.com>
2024-08-17 14:44:22 +0530

    Kitty randomizer

--- scripts/character/kitty_display.gd
+++ scripts/character/kitty_display.gd
@@ -5,9 +5,11 @@ class_name KittyDisplay
 
 enum KittyColor {SCRATCH, BLACK, WHITE, TUXEDO, CALICO, BROWNIE, BLUE, SILVER, POINT, GRAY}
 enum KittyFace {NEUTRAL, ANGRY, ANNOYED, EXCITED, ROCK}
+enum KittyRole {CUSTOMER, CASHIER, RESTOCKER}
 
 @export var kitty_color: KittyColor
 @export var kitty_face: KittyFace
+@export var kitty_role: KittyRole
 
 ## Change between walking animation and idle animaion...duh
 @export var is_walking = false
@@ -24,6 +26,7 @@ enum KittyFace {NEUTRAL, ANGRY, ANNOYED, EXCITED, ROCK}
 func _ready() -> void:
 	set_color(kitty_color)
 	set_face(kitty_face)
+	set_role(kitty_role)
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
@@ -45,7 +48,20 @@ func set_color(index: int) -> void:
 ## This one too
 func set_face(index: int) -> void:
 	face.region_rect.position.x = (index * 32)
-	
+
+## And finally this one, not fully functioning yet
+func set_role(index:int) -> void:
+	pass
+
+## This is what you asked for sir Hullahpop
+func randomize_look() -> void:
+	set_color(randi_range(0, KittyColor.size() - 1))
+	if randi_range(0, 4) == 1:
+		set_face(randi_range(1, KittyFace.size() - 1))
+	else:
+		set_face(0)
+	set_role(kitty_role)
+
 func set_flipped(arg: bool) -> void:
 	base.flip_h = arg
 	face.flip_h = arg
