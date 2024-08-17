a3a5f5c185c27c802626ddee4ed5ec3d49353402
scorpion2000 <robert.bartha1@gmail.com>
2024-08-17 14:08:00 +0200

    Handle empty counters

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -5,10 +5,18 @@ class_name Shop
 var shopShelves: Dictionary = {}	#dictionary
 var shopCounters: Dictionary = {}	#dictionary
 
+var emptyCounters: Array[Counter] = []
+
 func _registerShelf(shelf: Shelf):
 	var position: Vector2i = floor(shelf.global_position/32)
 	shopShelves[position] = shelf
 
-func _registerCounter(shelf: Counter):
-	var position: Vector2i = floor(shelf.global_position/32)
-	shopCounters[position] = shelf
+func _registerCounter(counter: Counter):
+	var position: Vector2i = floor(counter.global_position/32)
+	shopCounters[position] = counter
+
+func _handleEmptyCounter(counter: Counter):
+	if counter.counterEmpty:
+		emptyCounters.push_back(counter)
+	else:
+		emptyCounters.erase(counter)
