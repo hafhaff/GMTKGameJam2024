07923b0299a220e10e110db00460aa79a9c0c19b
scorpion2000 <robert.bartha1@gmail.com>
2024-08-17 16:08:59 +0200

    Handling empty counters at registry

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -6,14 +6,20 @@ var shopShelves: Dictionary = {}	#dictionary
 var shopCounters: Dictionary = {}	#dictionary
 
 var emptyCounters: Array[Counter] = []
+var tileMap: NavmeshUpdater = null
+
+func _registerTilemap(_tileMap: NavmeshUpdater):
+	tileMap = _tileMap
 
 func _registerShelf(shelf: Shelf):
 	var position: Vector2i = floor(shelf.global_position/32)
 	shopShelves[position] = shelf
+	tileMap._removeNavmeshFromTile(position)
 
 func _registerCounter(counter: Counter):
 	var position: Vector2i = floor(counter.global_position/32)
 	shopCounters[position] = counter
+	_handleEmptyCounter(counter)
 
 func _handleEmptyCounter(counter: Counter):
 	if counter.counterEmpty:
