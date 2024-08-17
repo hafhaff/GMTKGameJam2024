1fdc190dd0f0932b0932c6de690303a0515b3b33
scorpion2000 <robert.bartha1@gmail.com>
2024-08-17 11:09:35 +0200

    Enum fix
    oops

--- scripts/shelf_data.gd
+++ scripts/shelf_data.gd
@@ -30,7 +30,7 @@ func replaceItem (item, num):
 
 #Need this so I can move forward with the AI
 func fillWithRandom():
-	itemType = ItemGlobal.FoodTypes.find_key(randi_range(0, ItemGlobal.FoodTypes.size() - 1))
+	itemType = randi_range(0, ItemGlobal.FoodTypes.size() - 1)
 	itemNum = maxItemCount
 
 func _on_interact_shape_body_entered(body):
