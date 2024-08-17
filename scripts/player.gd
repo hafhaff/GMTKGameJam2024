3655ec514270693adbfe8e452fa14f5fc72265cb
Hafsa <101893711+hafhaff@users.noreply.github.com>
2024-08-17 14:09:03 -0400

    added zoom in/out

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,12 +4,17 @@ class_name Player
 
 @export var speed: float = 100
 @export var pickUpCoolDown = 0.3
+@export var zoom_min = Vector2(0.5000001, 0.5000001)
+@export var zoom_max = Vector2(6.0000001, 6.0000001)
 
 var heldItem : Node
 var counter: Counter
 var heldItemNum
 var canUnload
 
+var zoom_speed = Vector2(0.3000001, 0.3000001)
+
+@onready var camera = $Camera2D
 @onready var kittyDisplay: KittyDisplay = $KittyDisplay
 
 func _ready():
@@ -58,4 +63,16 @@ func pickUpBox (box):
 	box.disablePickup()
 	heldItem = box
 	print(heldItem.getName())
+	
+
+func _input(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				if camera.zoom > zoom_min:
+					camera.zoom -= zoom_speed
+			if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				if camera.zoom < zoom_min:
+					camera.zoom += zoom_speed
+	pass
 	
