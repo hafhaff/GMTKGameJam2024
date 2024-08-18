562145baee84f79a1a997b2db7ec96a7e32f1539
scorpion2000 <robert.bartha1@gmail.com>
2024-08-18 08:50:47 +0200

    Fix

--- scripts/navmesh_updater.gd
+++ scripts/navmesh_updater.gd
@@ -8,7 +8,7 @@ func _ready():
 	global_shop._registerTilemap(self)
 
 func _removeNavmeshFromTile(vector: Vector2i):
-	set_cell(vector, 0, Vector2i(0,0))
+	set_cell(vector, 0, Vector2i(4,3))
 
 func _addNavmeshToTile(vector: Vector2i):
-	set_cell(vector, 3, Vector2i(0,0))
+	set_cell(vector, 1, Vector2i(4,3))
