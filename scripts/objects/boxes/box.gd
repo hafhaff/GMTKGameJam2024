6c4134d9ad2527ceeed21a302fdc37a5c9562f57
NathanTheCraziest <nathanthecraziest@gmail.com>
2024-08-18 16:12:11 +0530

    Added Box variants

--- scripts/objects/boxes/box.gd
+++ scripts/objects/boxes/box.gd
@@ -1,3 +1,4 @@
+@tool
 extends Area2D
 
 class_name Box
@@ -5,16 +6,21 @@ class_name Box
 @export var itemNum = 10
 @export var itemType : ItemGlobal.FoodTypes
 
+@onready var base: Sprite2D = $Base
+
 signal selfYeet(Box)	#Called on self destruction
 signal pickedUp(Box, bool)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global_shop._addBox(self)
-
+	update_box_type(self.itemType)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#update box type in editor
+	if Engine.is_editor_hint():
+		update_box_type(self.itemType)
 	
 func getItemName():
 	print(itemType)
@@ -48,7 +54,6 @@ func _on_body_entered(body):
 	else: 
 		pass # Replace with function body.
 
-
 func _on_timer_timeout():
 	self.enablePickup()
 	pass # Replace with function body.
@@ -56,3 +61,7 @@ func _on_timer_timeout():
 func removeSelf():
 	selfYeet.emit(self)
 	queue_free()
+
+# Customization
+func update_box_type(index: int) -> void:
+	base.region_rect.position.x = index * 32 
