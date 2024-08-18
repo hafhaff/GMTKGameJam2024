8f89c7027ca20ecdbc7ac010b06f6d28d538f960
scorpion2000 <robert.bartha1@gmail.com>
2024-08-18 19:08:21 +0200

    Construction system finished

--- scripts/ui/construction_menu.gd
+++ scripts/ui/construction_menu.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var largeKitten: KittyDisplay = $KittyDisplay
 @onready var smolKittenHolder: Node = $"Construction Kitties"
+@onready var selectionPositioning: Node2D = $SelectionPositioning
+@onready var selectionSprite: Sprite2D = $SelectionPositioning/Sprite2D
 
 @export var selections: Array[PackedScene]
 @export var selectionDisplay: SelectionDisplay
@@ -9,8 +11,11 @@ extends Node
 var visible = true
 var largeKittenHidePos: Vector2 = Vector2(-130, 784)	#Yup, shit's hardcoded
 var largeKittenPos: Vector2 = Vector2(96,784)
+var selectionPos: Vector2 = Vector2(620,593)
+var selectionHiddenPos: Vector2 = Vector2(620,793)
 var constPos: Vector2
 var selection: PackedScene
+var selectionNum = 0
 
 func _ready():
 	_toggleDisplay()
@@ -30,12 +35,21 @@ func _input(event: InputEvent):
 			_toggleDisplay()
 		if event.is_action_pressed('construct'):
 			_build()
+		if event.is_action_pressed('construction_next'):
+			_selectionChange(true)
+		if event.is_action_pressed('construction_prev'):
+			_selectionChange(false)
 
 func _toggleDisplay():
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_QUAD)
 	tween.set_ease(Tween.EASE_OUT)
 	tween.tween_property(largeKitten, "global_position", largeKittenHidePos if visible else largeKittenPos, 0.5)
+
+	var tweenTheSecond = create_tween()
+	tweenTheSecond.set_trans(Tween.TRANS_QUAD)
+	tweenTheSecond.set_ease(Tween.EASE_OUT)
+	tweenTheSecond.tween_property(selectionPositioning, "global_position", selectionHiddenPos if visible else selectionPos, 0.5)
 
 	for child in smolKittenHolder.get_children():
 		child._hideKitty(visible)
@@ -44,7 +58,7 @@ func _toggleDisplay():
 	selectionDisplay.visible = visible
 
 func _build():
-	if selectionDisplay.selectionSprite.modulate != Color.GREEN:
+	if selectionDisplay.selectionSprite.modulate != Color.GREEN || !visible:
 		return
 	var building = selection.instantiate()
 	building.global_position = constPos * 32
@@ -66,3 +80,17 @@ func _spriteSetup(pos: Vector2i, parentPos: Vector2i):
 		if (right != parentPos): _spriteSetup(right, pos)
 		global_shop.shopShelves[pos].sprite.region_rect.position.x = 32
 		return
+
+func _selectionChange(next: bool):
+	if !visible:
+		return
+	if next:
+		if selectionNum + 1 <= selections.size()-1:
+			selectionNum += 1
+			selection = selections[selectionNum]
+	else:
+		if selectionNum - 1 >= 0:
+			selectionNum -= 1
+			selection = selections[selectionNum]
+	selectionDisplay.selectionSprite.texture = selectionDisplay.selectionSprites[selection]
+	selectionSprite.texture = selectionDisplay.selectionSprite.texture
