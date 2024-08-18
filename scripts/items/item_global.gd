5bc8add04d23be1c1dc6d0c2edeeeb708062a8a9
NathanTheCraziest <nathanthecraziest@gmail.com>
2024-08-18 15:34:30 +0530

    Added Catnip to item gloabl

--- scripts/items/item_global.gd
+++ scripts/items/item_global.gd
@@ -2,9 +2,10 @@ extends Node
 
 class_name ItemGlobal
 
-enum FoodTypes {CANNED, CEREAL}
+enum FoodTypes {CANNED, CEREAL, CATNIP}
 
 var FoodValues: Dictionary = {
 	FoodTypes.CANNED : {purchasePrice = 1, sellPrice = 2},
-	FoodTypes.CEREAL : {purchasePrice = 1, sellPrice = 2}
+	FoodTypes.CEREAL : {purchasePrice = 1, sellPrice = 2},
+	FoodTypes.CATNIP : {purchasePrice = 1, sellPrice = 2}
 }
