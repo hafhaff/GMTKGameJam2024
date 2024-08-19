0c6fa95556d2c52624e7b79aacf0cee2a39c19cc
scorpion2000 <robert.bartha1@gmail.com>
2024-08-19 20:41:42 +0200

    Initial pause commit

---
+++ scripts/ui/pause.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@export var mainMenuScene = "res://scenes/main_menu_concept.tscn"
+@export var switchSoundPlayer: AudioStreamPlayer
+@export var selectSoundPlayer: AudioStreamPlayer
+@export var optionsMenu: Panel
+
+func _ready():
+	for child in get_node("Panel").get_children():
+			for node in child.get_children():
+				if node is Button:
+					node.connect("mouse_entered", _playSwitch)
+					node.connect("pressed", _playSelect)
+
+func _input(event):
+	if event.is_action_pressed("ui_pause"):
+		pause()
+
+func pause():
+	visible = !get_tree().paused
+	get_tree().paused = !get_tree().paused
+
+func _return_to_menu():
+	get_tree().paused = !get_tree().paused
+	get_tree().change_scene_to_file(mainMenuScene)
+
+func _playSwitch():
+	if switchSoundPlayer.playing:
+		switchSoundPlayer.stop()
+	switchSoundPlayer.play()
+
+func _playSelect():
+	if selectSoundPlayer.playing:
+		selectSoundPlayer.stop()
+	selectSoundPlayer.play()
+
+func changeMenu():
+	optionsMenu.visible = !optionsMenu.visible
