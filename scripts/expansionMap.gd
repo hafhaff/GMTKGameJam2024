4c7fc49cd6f26172fb43dbbeee5fe9b0673a33ef
scorpion2000 <robert.bartha1@gmail.com>
2024-08-19 09:41:11 +0200

    Fixed navigation removal

--- scripts/expansionMap.gd
+++ scripts/expansionMap.gd
@@ -53,7 +53,7 @@ func _ready():
 	global_shop._registerTilemap(self)
 
 func _removeNavmeshFromTile(vector: Vector2i):
-	set_cell(vector, 0, Vector2i(4,3))
+	set_cell(vector, 1, Vector2i(4,3))
 
 func _addNavmeshToTile(vector: Vector2i):
-	set_cell(vector, 1, Vector2i(4,3))
+	set_cell(vector, 0, Vector2i(4,3))
