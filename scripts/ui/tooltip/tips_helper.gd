83cc786fb10427f98a14c357f78b86797a27fd75
NathanTheCraziest <nathanthecraziest@gmail.com>
2024-08-20 19:12:44 +0530

    freezer

--- scripts/ui/tooltip/tips_helper.gd
+++ scripts/ui/tooltip/tips_helper.gd
@@ -4,7 +4,7 @@ class_name TipsHelper
 
 var storageUnitTooltip: StorageTooltip = null
 var controlTips: ControlTips = null
-var item_names: Array = ["Canned Food", "Cereal", "Catnip"]
+var item_names: Array = ["Canned Food", "Cereal", "Catnip", "Milk", "Drinks", "Ice Cream"]
 
 func _registerStorageUnitTooltip(tooltip: StorageTooltip):
 	storageUnitTooltip = tooltip
