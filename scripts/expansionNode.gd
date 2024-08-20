4753399a549a7da148db48365d6b961ef5732f59
Mahdi <110603934+MahdiHassen@users.noreply.github.com>
2024-08-20 01:16:21 -0400

    expansion test

--- scripts/expansionNode.gd
+++ scripts/expansionNode.gd
@@ -193,7 +193,7 @@ func fixWalls():
 
 func rightExpansion(body):
 	if body is Player and (global_shop.getKitcoin() > price) and enabled:
-		global_shop.buy(global_shop.prices["expansion"])
+		global_shop.buy(global_shop.prices["expansion"] * global_shop.chunkNum * 4)
 		price = price * numExpansions
 		expand()
 		numExpansions = numExpansions + 1
