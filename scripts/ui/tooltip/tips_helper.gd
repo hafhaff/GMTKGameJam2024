e41df7d300412221a2bfb5d8ebcaacc8ad26ab25
NathanTheCraziest <nathanthecraziest@gmail.com>
2024-08-22 10:50:36 +0530

    Lose screen polished

--- scripts/ui/tooltip/tips_helper.gd
+++ scripts/ui/tooltip/tips_helper.gd
@@ -5,6 +5,7 @@ class_name TipsHelper
 var storageUnitTooltip: StorageTooltip = null
 var controlTips: ControlTips = null
 var item_names: Array = ["Canned Food", "Cereal", "Catnip", "Milk", "Drinks", "Ice Cream"]
+var player_appearance_data: Array = [0, 0]
 
 func _registerStorageUnitTooltip(tooltip: StorageTooltip):
 	storageUnitTooltip = tooltip
